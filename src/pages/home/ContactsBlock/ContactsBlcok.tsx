bf1b2df9c60c5164db4240dab9d200d440b32987
vladzyabs <59650523+vladzyabs@users.noreply.github.com>
2020-12-10 21:47:27 +0300

    added contacts block

--- src/pages/home/ContactsBlock/ContactsBlcok.tsx
+++ src/pages/home/ContactsBlock/ContactsBlcok.tsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faPhoneAlt, faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faPhoneAlt, faEnvelope, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons'
 import { Container, Title } from '../../../components/common'
 
 const ContactsBlock: React.FC = () => {
 	return (
 		<section className={'contacts'}>
 			<Container>
-				<Title title={'Contacts'} supTitle={'contact with me'} className={'contacts__title'}/>
 				<div className="contacts__content">
 
-					<div>
-						<FontAwesomeIcon icon={faPhoneAlt}/><a href="tel:+375336288208">+375 (33) 628-82-08</a>
-					</div>
+					<a href="tel:+375336288208" className={'contacts__item'}>
+						<h5 className={'contacts__item-title'}>
+							<FontAwesomeIcon icon={faPhoneAlt}/>Phone
+						</h5>
 
-					<div>
-						<FontAwesomeIcon icon={faEnvelope}/><a href="mailto:vladzyaba@gmail.com">vladzyaba@gmail.com</a>
-					</div>
+						<span className={'contacts__item-value'}>+375 (33) 628-82-08</span>
+					</a>
 
-					
+					<a href="mailto:vladzyaba@gmail.com" className={'contacts__item'}>
+						<h5 className={'contacts__item-title'}>
+							<FontAwesomeIcon icon={faEnvelope}/>Mail
+						</h5>
+
+						<span className={'contacts__item-value'}>vladzyaba@gmail.com</span>
+					</a>
+
+					<div className={'contacts__item'}>
+						<h5 className={'contacts__item-title'}>
+							<FontAwesomeIcon icon={faMapMarkedAlt}/>Address
+						</h5>
+
+						<address className={'contacts__item-value'}>Belarus, Mogilev</address>
+					</div>
 
 				</div>
 			</Container>
